ca06b910816c15acae88d349632e8833a8c0576f
Riley1101 <arkardev00@gmail.com>
2024-02-16 22:54:03 -0500

    added clang support and general lsp key bindings

--- lua/gideon/plugins/lsp.lua
+++ lua/gideon/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
         local lspconfig = require('lspconfig')
         lspconfig.pyright.setup {}
         lspconfig.tsserver.setup {}
+        lspconfig.svelte.setup {}
         lspconfig.clangd.setup {}
         lspconfig.rust_analyzer.setup {
             -- Server-specific settings. See `:help lspconfig-setup`
@@ -37,6 +38,8 @@ return {
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
                 vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
@@ -44,8 +47,6 @@ return {
                 vim.keymap.set('n', '<space>wl', function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, opts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', '<space>f', function()
                     vim.lsp.buf.format { async = true }
                 end, opts)
