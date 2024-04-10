bb177147eab040d8b025dd305668754f46a15f5a
Arkar <arkar@pop-os.lan>
2024-04-09 20:17:26 -0400

    update on tab indent

--- lua/gideon/core/sets.lua
+++ lua/gideon/core/sets.lua
@@ -1,3 +1,4 @@
+vim.g.netrw_banner = 0
 vim.opt.guicursor = ""
 vim.opt.spelllang = "en_us"
 vim.opt.nu = true
@@ -17,4 +18,20 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
-vim.g.netrw_banner = 0
+-- change font
+-- vim.opt.guifont = "Fira Code"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+-- expand tabs
+vim.o.expandtab = true
+
+vim.filetype.add({
+	extension = {
+		astro = "astro",
+	},
+})
