1caa6b1be102a4f186e0e230a7d564a0511cc30d
Riley1101 <arkardev00@gmail.com>
2025-04-16 21:59:15 -0400

    added prompt

--- lua/gideon/plugins/gideon.lua
+++ lua/gideon/plugins/gideon.lua
@@ -1,7 +1,17 @@
+-- lazy
 return {
-  "Riley1101/gideon.nvim",
-  config = function()
-    local gideon = require("gideon")
-    gideon.setup()
-  end,
+	"Riley1101/gideon.nvim",
+	config = function()
+		local system_prompt = [[
+        You are a intelligent coding partner, your job is to assist user in his coding project. You will have to sometimes provide what user is asked in correct and working code snippets. Sometimes users will ask to do basic task like renaming variables and rewriting in different format.
+        For example, rename all object to some key etc in that case you just copy and repaste the result without providing code snippets.
+        For example if user ask like quick sort in js and you only give the snippet without any text. Do not add any examples or how to use just provide a correct test.
+        Remember always references yourself as "Gideon". And paste the code snippet without markdown formatting.
+    ]]
+		local gideon = require("gideon")
+		gideon.setup({
+			prompt = system_prompt,
+			model = "gemini", -- "deepseek" for deepseek model
+		})
+	end,
 }
