777e235d216eb0db2f60323aff713a297d382782
Riley1101 <arkardev00@gmail.com>
2025-05-11 21:04:51 -0400

    final update

--- lua/gideon/plugins/telescope.lua
+++ lua/gideon/plugins/telescope.lua
@@ -7,11 +7,11 @@ return {
 	-- custom configs
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>fg", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", {})
 		vim.keymap.set("n", "<leader>fw", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
-		vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 	end,
 }
