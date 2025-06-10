21c0b285a959171a9b19d3f21ccf42d74ccb23e2
Riley1101 <arkardev00@gmail.com>
2025-06-09 21:39:16 -0400

    updated plugins

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,4 +1,7 @@
 vim.lsp.enable('pyright')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('tsserver')
+vim.lsp.enable('biome')
+vim.lsp.enable('svelte')
+vim.lsp.enable('rust_analyzer')
 
--- lua/plugins/neovim-lsp.lua
+++ lua/plugins/neovim-lsp.lua
@@ -4,7 +4,7 @@ return {
     vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
     vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
     vim.keymap.set("n", "<space>df", vim.diagnostic.open_float)
-    vim.keymap.set("n", "<space>dl", vim.diagnostic.setloclist)
+    vim.keymap.set("n", "<space>fd", vim.diagnostic.setloclist)
 
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
