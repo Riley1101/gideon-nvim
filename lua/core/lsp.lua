443bac4ee972f729c14deeb25b9be15b6b11097f
Riley1101 <arkardev00@gmail.com>
2025-10-06 21:53:07 -0400

    update neovim

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,6 +1,10 @@
 vim.lsp.enable('pyright')
+vim.lsp.enable('gopls')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('tsserver')
 vim.lsp.enable('biome')
 vim.lsp.enable('svelte')
+vim.lsp.enable('clangd')
 vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('golangci_lint_ls')
+
